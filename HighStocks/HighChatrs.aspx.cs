dea7b93399c4ee68b7c2d4415d31708452ceb41c
abhay <abhayc.008@gmail.com>
2017-02-18 20:36:33 +0530

    Highstock

---
+++ HighStocks/HighChatrs.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HighStocks
+{
+    public partial class HighChatrs : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HighStocks/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HighStocks.Startup))]
+namespace HighStocks
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
